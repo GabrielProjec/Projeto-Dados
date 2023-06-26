cc6e6681e04f08168305e8482b75b1279812f407
GabrielProjec <gabriel.acesso3@gmail.com>
2023-06-26 17:24:38 -0300

    Projeto Dado

---
+++ app.js
@@ -0,0 +1,26 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1
+
+let randomDiceImage = 'dice' + randomNumber1 + '.png';
+
+let randomImageSource = 'img/' + randomDiceImage;
+
+let image1 = document.querySelectorAll('img')[0]
+
+image1.setAttribute('src', randomImageSource);
+
+/* player 2 */
+
+
+let randomNumber2 = Math.floor(Math.random() * 6) + 1
+
+let randomImageSource2 = 'img/dice' + randomNumber2 + '.png';
+
+document.querySelectorAll('img')[1].setAttribute('src', randomImageSource2)
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML = '🏆 jogador 1 ganhou'
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector('h1').innerHTML = '🏆 jogador 2 ganhou'
+} else {
+    document.querySelector('h1').innerHTML = 'joguem novamente'
+}
